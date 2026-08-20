6400293063f06b779daa3a20c366cd5721b666e8
MateoDi9z <dzmateozd@protonmail.com>
2026-08-20 19:22:19 -0300

    pipeline test

--- api/src/main/kotlin/edu/austral/splitit/server/ServerApplication.kt
+++ api/src/main/kotlin/edu/austral/splitit/server/ServerApplication.kt
@@ -2,10 +2,20 @@ package edu.austral.splitit.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello, World!"
+    }
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <p>SplitIt</p>;
+  return <p>SplitIt - Home!</p>;
 }
