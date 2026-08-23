9b6f983667304a63cb27e00c415de28cd1a284af
MateoDi9z <dzmateozd@protonmail.com>
2026-08-23 15:38:58 -0300

    feat: controller & endpoint tests

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
 
     // ========== Test ==========
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
