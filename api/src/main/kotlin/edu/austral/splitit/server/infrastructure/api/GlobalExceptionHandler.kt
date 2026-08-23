512cdcd7c42663cf78a2d3c44b4ce291066027a6
MateoDi9z <dzmateozd@protonmail.com>
2026-08-23 11:44:21 -0300

    feat: error messages

---
+++ api/src/main/kotlin/edu/austral/splitit/server/infrastructure/api/GlobalExceptionHandler.kt
@@ -0,0 +1,50 @@
+package edu.austral.splitit.server.infrastructure.api
+
+import edu.austral.splitit.server.application.exception.EmailAlreadyInUseException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.AuthenticationException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.HandlerMethodValidationException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(
+        MethodArgumentNotValidException::class,
+        HandlerMethodValidationException::class,
+        HttpMessageNotReadableException::class,
+    )
+    fun handleInvalidRequest(): ResponseEntity<ErrorMessage> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorMessage(INVALID_REQUEST_MESSAGE))
+
+    @ExceptionHandler(EmailAlreadyInUseException::class)
+    fun handleEmailAlreadyInUse(exception: EmailAlreadyInUseException): ResponseEntity<ErrorMessage> =
+        ResponseEntity
+            .status(HttpStatus.CONFLICT)
+            .body(ErrorMessage(exception.message ?: EMAIL_ALREADY_IN_USE_MESSAGE))
+
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpected(exception: Exception): ResponseEntity<ErrorMessage> {
+        if (exception is AccessDeniedException || exception is AuthenticationException) {
+            throw exception
+        }
+        logger.error("Unhandled exception", exception)
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorMessage(INTERNAL_ERROR_MESSAGE))
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+        private const val INVALID_REQUEST_MESSAGE = "Invalid request data"
+        private const val EMAIL_ALREADY_IN_USE_MESSAGE = "Email already in use"
+        private const val INTERNAL_ERROR_MESSAGE = "Internal server error"
+    }
+}
