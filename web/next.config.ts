57583d59c4829b7e1d63bdf1e4f7af682031c136
MateoDi9z <dzmateozd@protonmail.com>
2026-09-02 21:04:42 -0300

    fix: next config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 import { config as dotenvConfig } from "dotenv";
+import path from "path";
 
 dotenvConfig({ path: "../.env" });
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "../"),
   output: "standalone",
 };
 
