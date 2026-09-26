af0149f3959f11cdb6c93b0f53ddc5de6e8ced57
MateoDi9z <dzmateozd@protonmail.com>
2026-09-26 14:26:03 -0300

    refactor: extract shared SplitItLogo component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,19 +5,8 @@ import { useRouter } from "next/navigation";
 import { useSession } from "@/lib/use-session";
 import { Spinner } from "@/components/ui/spinner";
 import { PrimaryLink } from "@/components/primary-link";
+import { SplitItLogo } from "@/components/splitit-logo";
 import { ArrowRight } from "lucide-react";
-
-function SplitItLogo() {
-  return (
-    <div className="relative h-[34px] w-[74px]">
-      <div className="bg-primary absolute top-0 left-0 h-[34px] w-[57px] rounded-[8px]" />
-      <div className="absolute top-[3px] left-[calc(50%+1px)] flex -translate-x-1/2 items-center justify-center text-[24px] leading-[1.15] font-extrabold whitespace-nowrap">
-        <span className="text-primary-foreground">Split</span>
-        <span className="text-black">It</span>
-      </div>
-    </div>
-  );
-}
 
 const PREVIEW_MEMBERS = [
   { initial: "N", name: "Nicolas", badge: "recibe", amount: "+$ 28.500", positive: true },
